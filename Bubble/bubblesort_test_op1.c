d4c74d6da9ee0fa9e61a2cc7261f657762691668
SAC-213 <ascencioncruzsaul@gmail.com>
2026-03-11 08:18:01 -0600

    Update bubblesort_test_op1.c

--- Bubble/bubblesort_test_op1.c
+++ Bubble/bubblesort_test_op1.c
@@ -1,34 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tiempo.h"
 
+//**************************************************
+//Libreria necesaria para medir rendimiento en Linux
+#include "tiempo.h"
+//**************************************************
 void bubbleop1 (int A[],int n);
-
-int main(int argc, char const *argv[]){
+int main(int argc, char *argv[])
+{
+//********************************************************
+//Variables necesarias para poder medir el tiempo en Linux
+	double utime0, stime0, wtime0,utime1, stime1, wtime1;
+//********************************************************
+    int i;
     int n = atoi(argv[1]);
-    int *A;
-
-    //Apartar memoria para n números  enteros
-	A=malloc(n*sizeof(int));
-	if(A==NULL)
-	{
-		printf("\nError al intentar reservar memoria para %d elementos\n",n);		
-		exit(1);
-	}	
-
-    for(int i=0;i<n;i++)
-		scanf("%d",&A[i]);
-	
-	//Llamar al algoritmo
-    
-	bubbleop1(A,n);
-	
-	
-    for (int i = 0; i < n; i++)
+    int *A = malloc(sizeof(int) * n);
+    if (A == NULL)
     {
-        printf("%d\n",A[i]);
+        printf("\nError al reservar memoria para %d elementos\n", n);
+        exit(EXIT_FAILURE);
     }
 
-    return 0;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+//******************************************************************	
+//Iniciar el conteo del tiempo para las evaluaciones de rendimiento
+//uswtime(&utime0, &stime0, &wtime0);
+//******************************************************************
+    bubbleop1(A,n);
+//******************************************************************	
+//Evaluar los tiempos de ejecución 
+//uswtime(&utime1, &stime1, &wtime1);
+//******************************************************************
+    free(A);
+//***************************************************************************************************	
+//Cálculo del tiempo de ejecución del programa
+//	printf("\n");
+//	printf("real (Tiempo total)  %.10f s\n",  wtime1 - wtime0);
+//	printf("user (Tiempo de procesamiento en CPU) %.10f s\n",  utime1 - utime0);
+//	printf("sys (Tiempo en acciónes de E/S)  %.10f s\n",  stime1 - stime0);
+//	printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+//	printf("\n");
+//***************************************************************************************************		
+//***************************************************************************************************	
+//Mostrar los tiempos en formato exponecial
+//	printf("\n");
+	printf("real (Tiempo total)  %.10e s\n",  wtime1 - wtime0);
+	printf("user (Tiempo de procesamiento en CPU) %.10e s\n",  utime1 - utime0);
+	printf("sys (Tiempo en acciónes de E/S)  %.10e s\n",  stime1 - stime0);
+	printf("CPU/Wall   %.10f %% \n",100.0 * (utime1 - utime0 + stime1 - stime0) / (wtime1 - wtime0));
+	printf("\n");
+//***************************************************************************************************	
+return 0;
 }
 
 void bubbleop1 (int A[],int n){
